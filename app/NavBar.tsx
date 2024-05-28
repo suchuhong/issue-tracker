2615143d729441eae1bffb94b2e178793986d801
suchuhong <1442615457@qq.com>
2024-05-29 00:43:52 +0800

    build the navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from "next/link";
+import { PiBugBeetleFill } from "react-icons/pi";
+
+const NavBar = () => {
+
+    const links = [
+        { label: "Dashboard", href: "/" },
+        { label: "Issues", href: "/issues" },
+    ]
+
+    return (
+        <nav className='flex space-x-6 border-b mb-5 px-4 h-14 items-center'>
+            <Link href="/">
+                <PiBugBeetleFill />
+            </Link>
+            <ul className='flex space-x-6'>
+                {links.map((link) =>
+                <Link
+                    key={link.href}
+                    href={link.href}
+                    className='text-zinc-400 hover:text-zinc-800 transition-colors'>
+                    {link.label}
+                </Link>)}
+
+            </ul>
+        </nav>
+    )
+};
+
+export default NavBar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
+import React from "react";
+import NavBar from "@/app/NavBar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -10,13 +12,18 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+    <NavBar/>
+    <main>
+      {children}
+    </main>
+    </body>
     </html>
   )
 }
