27b8eb3bf0b1e0632a796d8d0e8f6e33ad2c9a69
suchuhong <1442615457@qq.com>
2024-06-03 00:45:45 +0800

    增加markdown编辑器

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,17 @@
-import { Button, TextArea, TextField } from '@radix-ui/themes'
+'use client';
+
+import { Button, TextField } from '@radix-ui/themes'
 import React from 'react'
+import "easymde/dist/easymde.min.css";
+import dynamic from 'next/dynamic'
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
 
 const NewIssuePage = () => {
   return (
     <div className='max-w-xl space-y-3'>
         <TextField.Root placeholder='title'>
         </TextField.Root>
-        <TextArea placeholder='Description' />
+        <SimpleMDE placeholder='Description' />
         <Button>Submit New Issue</Button>
     </div>
   )
