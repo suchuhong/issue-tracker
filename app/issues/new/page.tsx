c93d1a6ffdd60571c31c3b1c4044fce403e62171
suchuhong <1442615457@qq.com>
2024-06-03 23:16:59 +0800

    表单提交

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,17 +3,39 @@
 import { Button, TextField } from '@radix-ui/themes'
 import React from 'react'
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
 import dynamic from 'next/dynamic'
+import { useRouter } from 'next/navigation';
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+
+  const router = useRouter();
+  const {register, control, handleSubmit} = useForm<IssueForm>();
+
   return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root placeholder='title'>
+    <form className='max-w-xl space-y-3' 
+    onSubmit={
+      handleSubmit(async (data) => {
+        await axios.post('/api/issues', data);
+        router.push('/issues');
+      })
+    }>
+        <TextField.Root placeholder='title' {...register('title')}>
         </TextField.Root>
-        <SimpleMDE placeholder='Description' />
+        <Controller 
+          name= "description"
+          control={control}
+          render={ ({ field }) => <SimpleMDE placeholder='Description' {...field}/> }
+          />
         <Button>Submit New Issue</Button>
-    </div>
+    </form>
   )
 }
 
