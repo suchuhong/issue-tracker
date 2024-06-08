3fcebdd6ee17a2329327e702e06d1d3888cbbeac
suchuhong <1442615457@qq.com>
2024-06-08 21:27:26 +0800

    增加issue详情显示骨架和创建新issue骨架

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,21 @@
+import { Box, Card, Flex, Heading } from '@radix-ui/themes';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import React from 'react';
 
 const LoadingIssuedetailpage = () => {
-  return <div>LoadingIssuedetailpage</div>;
+  return (
+    <Box>
+      <Skeleton />
+      <Flex className='space-x-5' my='2'>
+        <Skeleton width='5rem' />
+        <Skeleton width='8rem' />
+      </Flex>
+      <Card className='prose' mt='4'>
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssuedetailpage;
