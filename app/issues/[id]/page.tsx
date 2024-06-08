178eb7314e1c5555c65448c10a6496f47cbde3d0
suchuhong <1442615457@qq.com>
2024-06-08 20:50:31 +0800

    修改issue展示的样式

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import React from 'react';
 
@@ -7,10 +9,10 @@ interface Props {
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  // localhost:3000/issues/a
+  // http://localhost:3000/issues/a
   // if (typeof params.id !== 'number') notFound();
 
-  http: const issue = await prisma.issues.findUnique({
+  const issue = await prisma.issues.findUnique({
     where: { id: parseInt(params.id) },
   });
 
@@ -18,10 +20,14 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex className='space-x-5' my='2'>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        <p>{issue.description}</p>
+      </Card>
     </div>
   );
 };
