4dc7b9ce30675d2f214c94072c12f52f2558fd41
suchuhong <1442615457@qq.com>
2024-06-09 20:18:35 +0800

    增加删除issue确认页面

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,13 +1,35 @@
 import { TrashIcon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import Link from 'next/link';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color='red'>
-      <TrashIcon />
-      <Link href={`/issues/edit/${issueId}`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'>
+          <TrashIcon /> Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth='450px'>
+        <AlertDialog.Title>删除</AlertDialog.Title>
+        <AlertDialog.Description size='2'>
+          确定删除这个issue？这个操作无法回退
+        </AlertDialog.Description>
+
+        <Flex gap='3' mt='4' justify='end'>
+          <AlertDialog.Cancel>
+            <Button variant='soft' color='gray'>
+              取消
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant='solid' color='red'>
+              确认
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
