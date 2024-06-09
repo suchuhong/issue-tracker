eedb627757ed63df163e2554f9ba23fd9bdcf3fa
suchuhong <1442615457@qq.com>
2024-06-09 20:41:00 +0800

    增加删除时异常处理

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -3,44 +3,70 @@ import { TrashIcon } from '@radix-ui/react-icons';
 import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
-  return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color='red'>
-          <TrashIcon /> Delete Issue
-        </Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content maxWidth='450px'>
-        <AlertDialog.Title>删除</AlertDialog.Title>
-        <AlertDialog.Description size='2'>
-          确定删除这个issue?这个操作无法回退
-        </AlertDialog.Description>
+  const [error, setError] = useState(false);
 
-        <Flex gap='3' mt='4' justify='end'>
-          <AlertDialog.Cancel>
-            <Button variant='soft' color='gray'>
-              取消
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              variant='solid'
-              color='red'
-              onClick={async () => {
-                await axios.delete('/api/issues/' + issueId);
-                router.push('/issues');
-                router.refresh();
-              }}
-            >
-              确认
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+  const deleteIssue = async () => {
+    try {
+      await axios.delete('/api/issues/' + issueId);
+      router.push('/issues');
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color='red'>
+            <TrashIcon /> Delete Issue
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content maxWidth='450px'>
+          <AlertDialog.Title>删除</AlertDialog.Title>
+          <AlertDialog.Description size='2'>
+            确定删除这个issue?这个操作无法回退
+          </AlertDialog.Description>
+
+          <Flex gap='3' mt='4' justify='end'>
+            <AlertDialog.Cancel>
+              <Button variant='soft' color='gray'>
+                取消
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant='solid' color='red' onClick={deleteIssue}>
+                确认
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content maxWidth='450px'>
+          <AlertDialog.Title>删除异常</AlertDialog.Title>
+          <AlertDialog.Description size='2'>
+            删除操作发生错误
+          </AlertDialog.Description>
+
+          <Button
+            variant='soft'
+            color='gray'
+            mt='2'
+            onClick={() => {
+              setError(false);
+            }}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
