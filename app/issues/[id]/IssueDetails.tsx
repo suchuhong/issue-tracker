9d04420aae064d39bf513ce31a5cd6c2f0645109
suchuhong <1442615457@qq.com>
2024-06-09 18:42:34 +0800

    分离详情和编辑

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,21 @@
+import { IssueStatusBadge } from '@/app/components';
+import { Issues } from '@prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import ReactMarkdown from 'react-markdown';
+
+const IssueDetails = ({ issue }: { issue: Issues }) => {
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+      <Flex className='space-x-5' my='2'>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className='prose' mt='4'>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
